adcac2e98fe0103ad12c201c4e4c1d714e25b251
Toni33812 <86938023+Toni33812@users.noreply.github.com>
2021-07-24 09:55:58 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 // Config
 var config = {
-    botName: '🔹 𝙉 O T 🔹',
-    operator: ['6281515860089'],
+    botName: 'IPEN BOT',
+    operator: ['6281367536917'],
     prefix: process.env.prefix ? new RegExp('^' + process.env.prefix) : /^[°•π÷×¶∆£¢€¥®™✓_=|~!?@#$%^&.\/\\©^]/,
     downloadStatus: false, // Curi Status Orang :|
     devMode: false, // true,
